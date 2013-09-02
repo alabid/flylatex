bdcf0b16e52ff27c0902b29b68873ab4dddd1881
Daniel Alabi <alabid@carleton.edu>
2013-09-01 23:13:43 -0400

    some more indentation fixing in 'configs.js'

--- configs.js
+++ configs.js
@@ -1,18 +1,18 @@
 var config = {
     pdfs: {
-    // absolute path of where to store compiled pdfs
-    // defaults to "pdfs" directory in "flylatex" repo
-    path: ""
+        // absolute path of where to store compiled pdfs
+        // defaults to "pdfs" directory in "flylatex" repo
+        path: ""
     }
     , db : {
-    url : "mongodb://localhost/flydb" // for example: mongodb://localhost/flydb3"
+        url : "mongodb://localhost/flydb" // for example: mongodb://localhost/flydb3"
     }, docs: {
-    // maximum number of documents per user
-    MAX_NUM_PER_USER : 20
+        // maximum number of documents per user
+        MAX_NUM_PER_USER : 20
     }, includes: {
-    // absolute path of includes
-    // defaults to "texpackages" directory in "flylatex" repo
-    path : ""
+        // absolute path of includes
+        // defaults to "texpackages" directory in "flylatex" repo
+        path : ""
     }
 };
 
